15c280b770036ba905fbf85ce361f6fd56face1b
Norihiko Itou <itounrh@jae.co.jp>
2026-01-09 16:10:40 +0900

    Apply Ether for STM32H755
    # Conflicts:
    #	arch/arm/src/stm32h7/hardware/stm32_ethernet.h

--- arch/arm/src/stm32h7/hardware/stm32_ethernet.h
+++ arch/arm/src/stm32h7/hardware/stm32_ethernet.h
@@ -32,6 +32,7 @@
  */
 
 #if defined(CONFIG_STM32H7_STM32H7X3XX) || \
+    defined(CONFIG_STM32H7_STM32H7X5XX) || \
     defined(CONFIG_STM32H7_STM32H7X7XX)
 
 /****************************************************************************
@@ -675,5 +676,5 @@ struct eth_desc_s
  ****************************************************************************/
 
 #endif /* __ASSEMBLY__ */
-#endif /* CONFIG_STM32H7_STM32H7X3XX */
+#endif /* CONFIG_STM32H7_STM32H7X3XX || CONFIG_STM32H7_STM32H7X5XX  || CONFIG_STM32H7_STM32H7X7XX */
 #endif /* __ARCH_ARM_SRC_STM32H7_HARDWARE_STM32_ETHERNET_H */
